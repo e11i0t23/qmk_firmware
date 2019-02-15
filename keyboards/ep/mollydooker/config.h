ec1d3b68193ef19b8402f5272e2ca34c2393f50d
Elliot Powell <johnp8727@gmail.com>
2019-02-15 22:36:35 +0000

    Implemented default keymap

--- keyboards/ep/mollydooker/config.h
+++ keyboards/ep/mollydooker/config.h
@@ -42,7 +42,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
 */
 #define MATRIX_ROW_PINS { F4, F5, F6, F7, D2 }
-#define MATRIX_COL_PINS { B0, B2, B3, E6, B7, F1, F0, D0, D1, D7, D5, D4, D6, B4, B5, D3, B6, C6, C7 }
+#define MATRIX_COL_PINS { B1, B2, B3, E6, B7, F1, F0, D0, D1, D7, D5, D4, D6, B4, B5, D3, B6, C6, C7 }
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL, or CUSTOM_MATRIX */
