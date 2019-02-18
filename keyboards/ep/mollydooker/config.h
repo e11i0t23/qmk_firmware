8c8028eeaad48f92e14cee89eeee5373f7a64ce1
Elliot Powell <johnp8727@gmail.com>
2019-02-18 20:47:27 +0000

    Update config

--- keyboards/ep/mollydooker/config.h
+++ keyboards/ep/mollydooker/config.h
@@ -19,6 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
+
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x9696
