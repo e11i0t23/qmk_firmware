74f80e76dc7de07020c8dabd98f36424f08c5129
Elliot Powell <johnp8727@gmail.com>
2019-02-22 04:05:17 +0000

    updates to stm32f072B files

--- keyboards/chibios_test/boards/ST_STM32F072B_DISCOVERY/board.mk
+++ keyboards/chibios_test/boards/ST_STM32F072B_DISCOVERY/board.mk
@@ -1,5 +1,9 @@
 # List of all the board related files.
-BOARDSRC = $(BOARD_PATH)/boards/ST_STM32F072B_DISCOVERY/board.c
+BOARDSRC = $(CHIBIOS)/os/hal/boards/ST_STM32F072B_DISCOVERY/board.c
 
 # Required include directories
-BOARDINC = $(BOARD_PATH)/boards/ST_STM32F072B_DISCOVERY
+BOARDINC = $(CHIBIOS)/os/hal/boards/ST_STM32F072B_DISCOVERY
+
+# Shared variables
+ALLCSRC += $(BOARDSRC)
+ALLINC  += $(BOARDINC)
