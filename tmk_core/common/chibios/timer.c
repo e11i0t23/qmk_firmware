79672e9fc3bbc6ce6803294eb9e183ee2160c497
Elliot Powell <johnp8727@gmail.com>
2019-03-02 19:49:32 +0000

    Removed overflow calculations

--- tmk_core/common/chibios/timer.c
+++ tmk_core/common/chibios/timer.c
@@ -2,50 +2,43 @@
 
 #include "timer.h"
 
-/*
-#ifndef ST2MS
-#define ST2MS(n) (((n) * 1000UL + CH_CFG_ST_FREQUENCY - 1UL) / CH_CFG_ST_FREQUENCY)
-#endif
-#ifndef MS2ST
-#define MS2ST(msec) ((systime_t)((((msec) * CH_CFG_ST_FREQUENCY - 1L) / 1000L) + 1L))
-#endif
-*/
-
-
 static systime_t last_systime = 0;
 static systime_t overflow = 0;
-static uint32_t current_time_ms = 0;
+static uint64_t current_time_ms = 0;
 
 void timer_init(void) {
-  timer_clear();
+	timer_clear();
 }
 
 void timer_clear(void) {
-  last_systime = chVTGetSystemTime();
-  overflow = 0;
-  current_time_ms = 0;
+	 last_systime = chVTGetSystemTime();
+	 overflow = 0;
+	 current_time_ms = 0;
 }
 
-uint16_t timer_read(void) {
-  return (uint16_t)timer_read32();
+uint16_t timer_read(void)
+{
+    return (uint16_t)timer_read32();
 }
 
-uint32_t timer_read32(void) {
-  // Note: We assume that the timer update is called at least once betweeen every wrap around of the system time
-  systime_t current_systime = chVTGetSystemTime();
-  systime_t elapsed = current_systime - last_systime + overflow;
-  uint32_t elapsed_ms = TIME_I2MS(elapsed);
-  current_time_ms += elapsed_ms;
-  overflow = elapsed - TIME_MS2I(elapsed_ms);
-  last_systime = current_systime;
+uint32_t timer_read32(void)
+{
+	systime_t current_systime = chVTGetSystemTime();
+	systime_t elapsed = current_systime - last_systime + overflow;
+	uint64_t elapsed_ms = TIME_I2MS(elapsed);
+	current_time_ms += elapsed_ms;
+//	overflow = elapsed - TIME_MS2I(elapsed_ms);
+	last_systime = current_systime;
 
-  return current_time_ms;
+    return (uint32_t)current_time_ms;
 }
 
-uint16_t timer_elapsed(uint16_t last) {
-  return timer_read() - last;
+uint16_t timer_elapsed(uint16_t last)
+{
+    return (uint16_t)(TIME_I2MS(chVTTimeElapsedSinceX(TIME_MS2I(last))));
 }
 
-uint32_t timer_elapsed32(uint32_t last) {
-  return timer_read32() - last;
+uint32_t timer_elapsed32(uint32_t last)
+{
+    return TIME_I2MS(chVTTimeElapsedSinceX(TIME_MS2I(last)));
 }
