42141f062945708ac15a75f5c31cac535fbbd326
Elliot Powell <johnp8727@gmail.com>
2019-03-04 17:57:28 +0000

    More timer changes

--- tmk_core/common/chibios/timer.c
+++ tmk_core/common/chibios/timer.c
@@ -2,35 +2,18 @@
 
 #include "timer.h"
 
-static systime_t last_systime = 0;
-static systime_t overflow = 0;
-static uint64_t current_time_ms = 0;
+void timer_init(void) {}
 
-void timer_init(void) {
-	timer_clear();
-}
-
-void timer_clear(void) {
-	 last_systime = chVTGetSystemTime();
-	 overflow = 0;
-	 current_time_ms = 0;
-}
+void timer_clear(void) {}
 
 uint16_t timer_read(void)
 {
-    return (uint16_t)timer_read32();
+    return (uint16_t)TIME_I2MS(chVTGetSystemTime());
 }
 
 uint32_t timer_read32(void)
 {
-	systime_t current_systime = chVTGetSystemTime();
-	systime_t elapsed = current_systime - last_systime + overflow;
-	uint64_t elapsed_ms = TIME_I2MS(elapsed);
-	current_time_ms += elapsed_ms;
-//	overflow = elapsed - TIME_MS2I(elapsed_ms);
-	last_systime = current_systime;
-
-    return (uint32_t)current_time_ms;
+    return TIME_I2MS(chVTGetSystemTime());
 }
 
 uint16_t timer_elapsed(uint16_t last)
