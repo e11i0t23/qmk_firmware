1a9363c7ab8beb62be92193b000cab7a60cf254f
Elliot Powell <johnp8727@gmail.com>
2020-04-07 16:21:13 +0100

    Via support

--- keyboards/ep/comsn/lets_split_hs/rules.mk
+++ keyboards/ep/comsn/lets_split_hs/rules.mk
@@ -33,6 +33,3 @@ SPLIT_KEYBOARD = yes
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
-
-LAYOUTS = ortho_4x12
-
