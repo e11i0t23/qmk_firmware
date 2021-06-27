1b2c941dfc875090407678eb3efb4d79e0544a13
Elliot Powell <johnp8727@gmail.com>
2021-06-27 23:02:59 +0100

    initial commit

---
+++ keyboards/ep/comsn/qbalsdon_macropad/keymaps/default/keymap.c
@@ -0,0 +1,83 @@
+/* Copyright 2021 Elliot Powell
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* Base */
+    [0] = LAYOUT(
+                 KC_2,    KC_3,     KC_4,   KC_5,
+                          KC_3,    KC_4,     KC_5,
+        KC_1,    KC_2,    KC_3,     KC_4,   KC_5,
+        KC_1,    KC_2,    KC_3,     KC_4,   KC_5,
+        KC_1,    KC_2,    KC_3,     KC_4,   KC_5,
+        KC_1,    KC_2,    KC_3,     KC_4,   KC_5,
+        KC_1,    KC_2,    KC_3,     KC_4,   KC_5
+    ),
+};
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) { /* First encoder */
+        if (clockwise) {
+            tap_code(KC_PGDN);
+        } else {
+            tap_code(KC_PGUP);
+        }
+    } else if (index == 1) { /* Second encoder */
+        if (clockwise) {
+            tap_code(KC_DOWN);
+        } else {
+            tap_code(KC_UP);
+        }
+    } else if (index == 2) { /* Second encoder */
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
+    }
+    return true;
+}
+
+const rgblight_segment_t PROGMEM my_capslock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {6, 1, HSV_RED}       // Light 1 LEDs, starting with LED 6
+);
+const rgblight_segment_t PROGMEM my_layer0_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {9, 1, HSV_CYAN},
+    {10, 1, HSV_GREEN},
+    {11, 1, HSV_PURPLE}
+);
+
+// Now define the array of layers. Later layers take precedence
+const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
+    my_capslock_layer,
+    my_layer0_layer
+);
+
+void keyboard_post_init_user(void) {
+    // Enable the LED layers
+    rgblight_layers = my_rgb_layers;
+}
+
+bool led_update_user(led_t led_state) {
+    rgblight_set_layer_state(0, led_state.caps_lock);
+    return true;
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    rgblight_set_layer_state(1, layer_state_cmp(state, 0));
+    return state;
+}
