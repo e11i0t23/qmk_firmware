9c1c1a06796042af6318c2da6b15ca66bf7c3f50
Elliot Powell <johnp8727@gmail.com>
2021-06-28 20:34:22 +0100

    add layers and rgb colouring for them

--- keyboards/ep/comsn/qbalsdon_macropad/keymaps/default/keymap.c
+++ keyboards/ep/comsn/qbalsdon_macropad/keymaps/default/keymap.c
@@ -19,13 +19,58 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [0] = LAYOUT(
-                 KC_2,    KC_3,     KC_4,   LT(1, KC_MUTE),
-                          KC_3,    KC_4,     KC_5,
+                 KC_2,    KC_3,     KC_4,   KC_MUTE,
+                          MO(3),    TO(1),  TO(2),
         KC_1,    KC_2,    KC_3,     KC_4,   KC_5,
         KC_1,    KC_2,    KC_3,     KC_4,   KC_5,
         KC_1,    KC_2,    KC_3,     KC_4,   KC_5,
         KC_1,    KC_2,    KC_3,     KC_4,   KC_5,
         KC_1,    KC_2,    KC_3,     KC_4,   KC_5
+    ),
+    [1] = LAYOUT(
+                    KC_TRNS,    KC_TRNS,     KC_TRNS,   KC_TRNS,
+                                TO(0),       TO(1),     TO(2),
+        KC_ESC,    KC_TRNS,     KC_TRNS,     KC_TRNS,   KC_ENTER,
+        S(C(KC_A)), C(KC_1),    S(C(KC_P)),  A(KC_N),   KC_TRNS,
+        KC_TRNS,    KC_TRNS,    KC_TRNS,     KC_TRNS,   KC_TRNS,
+        KC_TRNS,    KC_TRNS,    KC_TRNS,     KC_TRNS,   KC_TRNS,
+        KC_TRNS,    KC_TRNS,    KC_TRNS,     KC_TRNS,   KC_TRNS
+    ),
+    [2] = LAYOUT(
+                    KC_TRNS,    KC_TRNS,     KC_TRNS,   KC_TRNS,
+                                TO(0),       TO(1),     TO(2),
+        KC_TRNS,    KC_TRNS,    KC_TRNS,     KC_TRNS,   KC_TRNS,
+        KC_TRNS,    KC_TRNS,    KC_TRNS,     KC_TRNS,   KC_TRNS,
+        KC_TRNS,    KC_TRNS,    KC_TRNS,     KC_TRNS,   KC_TRNS,
+        KC_TRNS,    KC_TRNS,    KC_TRNS,     KC_TRNS,   KC_TRNS,
+        KC_TRNS,    KC_TRNS,    KC_TRNS,     KC_TRNS,   KC_TRNS
+    ),
+    [3] = LAYOUT( // TAP LAYER
+                    KC_TRNS,    KC_TRNS,     KC_TRNS,   KC_TRNS,
+                                KC_TRNS,     TO(4),     TO(5),
+        KC_TRNS,    KC_TRNS,    KC_TRNS,     KC_TRNS,   KC_TRNS,
+        KC_TRNS,    KC_TRNS,    KC_TRNS,     KC_TRNS,   KC_TRNS,
+        KC_TRNS,    KC_TRNS,    KC_TRNS,     KC_TRNS,   KC_TRNS,
+        KC_TRNS,    KC_TRNS,    KC_TRNS,     KC_TRNS,   KC_TRNS,
+        KC_TRNS,    KC_TRNS,    KC_TRNS,     KC_TRNS,   KC_TRNS
+    ),
+    [4] = LAYOUT(
+                    KC_TRNS,    KC_TRNS,     KC_TRNS,   KC_TRNS,
+                                TO(0),       TO(1),     TO(5),
+        KC_TRNS,    KC_TRNS,    KC_TRNS,     KC_TRNS,   KC_TRNS,
+        KC_TRNS,    KC_TRNS,    KC_TRNS,     KC_TRNS,   KC_TRNS,
+        KC_TRNS,    KC_TRNS,    KC_TRNS,     KC_TRNS,   KC_TRNS,
+        KC_TRNS,    KC_TRNS,    KC_TRNS,     KC_TRNS,   KC_TRNS,
+        KC_TRNS,    KC_TRNS,    KC_TRNS,     KC_TRNS,   KC_TRNS
+    ),
+    [5] = LAYOUT(
+                    KC_TRNS,    KC_TRNS,     KC_TRNS,   KC_TRNS,
+                                TO(0),       TO(4),     TO(2),
+        KC_TRNS,    KC_TRNS,    KC_TRNS,     KC_TRNS,   KC_TRNS,
+        KC_TRNS,    KC_TRNS,    KC_TRNS,     KC_TRNS,   KC_TRNS,
+        KC_TRNS,    KC_TRNS,    KC_TRNS,     KC_TRNS,   KC_TRNS,
+        KC_TRNS,    KC_TRNS,    KC_TRNS,     KC_TRNS,   KC_TRNS,
+        KC_TRNS,    KC_TRNS,    KC_TRNS,     KC_TRNS,   KC_TRNS
     ),
 };
 
@@ -68,15 +113,33 @@ const rgblight_segment_t PROGMEM my_capslock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
     {6, 1, HSV_RED}       // Light 1 LEDs, starting with LED 6
 );
 const rgblight_segment_t PROGMEM my_layer0_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {9, 1, HSV_CYAN},
-    {10, 1, HSV_GREEN},
-    {11, 1, HSV_PURPLE}
+    {11, 1, HSV_GREEN}
+);
+const rgblight_segment_t PROGMEM my_layer1_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {10, 1, HSV_GREEN}
+
+);const rgblight_segment_t PROGMEM my_layer2_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {9, 1, HSV_GREEN}
+);
+const rgblight_segment_t PROGMEM my_layer3_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {11, 1, HSV_CYAN}
+);
+const rgblight_segment_t PROGMEM my_layer4_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {10, 1, HSV_PURPLE}
+
+);const rgblight_segment_t PROGMEM my_layer5_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {9, 1, HSV_PURPLE}
 );
 
 // Now define the array of layers. Later layers take precedence
 const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
     my_capslock_layer,
-    my_layer0_layer
+    my_layer0_layer,
+    my_layer1_layer,
+    my_layer2_layer,
+    my_layer3_layer,
+    my_layer4_layer,
+    my_layer5_layer
 );
 
 void keyboard_post_init_user(void) {
@@ -91,6 +154,11 @@ bool led_update_user(led_t led_state) {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     rgblight_set_layer_state(1, layer_state_cmp(state, 0));
+    rgblight_set_layer_state(2, layer_state_cmp(state, 1));
+    rgblight_set_layer_state(3, layer_state_cmp(state, 2));
+    rgblight_set_layer_state(4, layer_state_cmp(state, 3));
+    rgblight_set_layer_state(5, layer_state_cmp(state, 4));
+    rgblight_set_layer_state(6, layer_state_cmp(state, 5));
     return state;
 }
 
