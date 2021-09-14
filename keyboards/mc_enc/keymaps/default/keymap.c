19fa97a18adcff376db6ec4d2afefc6768324b41
Elliot Powell <johnp8727@gmail.com>
2021-09-14 16:59:04 +0100

    keymap updgrade

--- keyboards/mc_enc/keymaps/default/keymap.c
+++ keyboards/mc_enc/keymaps/default/keymap.c
@@ -14,7 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
-
+uint8_t encoder_mode = 0;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
@@ -23,14 +23,73 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 
+void set_lighting(void) {
+        switch(encoder_mode)
+            {
+            case 3*64:
+                rgblight_sethsv(HSV_CYAN);
+                break;
+            case 1*64:
+                rgblight_sethsv(HSV_GREEN);
+                break;
+            case 2*64:
+                rgblight_sethsv(HSV_RED);
+                break;
+            case 0*64:
+                rgblight_sethsv(HSV_ORANGE);
+                break;
+            default:
+                rgblight_sethsv(HSV_PURPLE);
+                break;
+            }
+}
+
+void post_process_record_user(uint16_t keycode, keyrecord_t *record) {
+    set_lighting();
+}
+
 void encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) { /* First encoder */
-        switch(biton32(layer_state)){
-            case 1:
+        switch (biton32(layer_state))
+        {
+        case 1:
                 if (clockwise) {
-                    tap_code16(G(S(KC_A)));
+                    encoder_mode += 64;
                 } else {
+                    encoder_mode += -64;
+                }
+                set_lighting();
+            break;
+
+        default:
+            switch(encoder_mode)
+            {
+            case 3*64:
+                if (clockwise) {
+                    tap_code(KC_VOLU);
+                } else {
+                    tap_code(KC_VOLD);
+                }
+                break;
+            case 1*64:
+                if (clockwise) {
+                    tap_code(KC_RIGHT);
+                } else {
+                    tap_code(KC_LEFT);
+                }
+                break;
+            case 2*64:
+                if (clockwise) {
+                    tap_code(KC_PGDN);
+                } else {
+                    tap_code(KC_PGUP);
+                }
+                break;
+            case 0*64:
+                if (clockwise) {
                     tap_code(KC_DOWN);
+                } else {
+                    tap_code(KC_UP);
                 }
                 break;
             default:
@@ -40,6 +99,8 @@ void encoder_update_user(uint8_t index, bool clockwise) {
                     tap_code(KC_VOLD);
                 }
                 break;
-      }
+            }
+            break;
+        }
     }
 }
